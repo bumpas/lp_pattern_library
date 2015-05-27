09c57aaae4118f0e22e21e4b987828e063c98070
Bumpas <andrew.bumpas@otterproducts.com>
2015-05-27 17:16:34 -0600

    module23split update, started module50pad
    - small update to the video on module23split
    - start to module50pad
    - updated video
    - added hidetext mixin
    - updated typography for select and p line height
    
    Signed-off-by: Bumpas <andrew.bumpas@otterproducts.com>

--- pattern-library/js/app/app.js
+++ pattern-library/js/app/app.js
@@ -19,20 +19,34 @@ $(function(){
 		nav.append(item);
 	});
 
+	//slick plugin for small screen sizes < 768px
+	$('.slick--mobile').slick({
+		mobileFirst: true,
+		arrows: false,
+		dots: true,
+		dotsClass: "slickDots slickDots--default",
+		responsive: [
+			{
+	  			breakpoint: 768,
+	  			settings: "unslick"
+			}
+		]
+	});
 
-});
-//slick plugin for small screen sizes only < 768px
-$(document).ready(function(){
-  $('.slick--mobile').slick({
-  	mobileFirst:true,
-  	arrows: false,
-  	dots: true,
-  	dotsClass: "slickDots slickDots--default",
-    responsive: [
-    {
-      breakpoint: 768,
-      settings: "unslick"
-    }
-  ]
-  });
+	//slick plugin for module50pad
+	$('.slick--module50pad').slick({
+		mobileFirst: true,
+		arrowsClass: "slickArrows",
+		dots: true,
+		dotsClass: "slickDots slickDots--default",
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					row: 2,
+					slidesToShow: 2
+				}
+			}
+		]
+	});
 });
