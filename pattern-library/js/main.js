f83342fc31c98c3e9071859a10a55f6c3e0c4d82
Bumpas <andrew.bumpas@otterproducts.com>
2015-06-18 10:11:22 -0600

    youtube for module50pad and module23split
    Signed-off-by: Bumpas <andrew.bumpas@otterproducts.com>

--- pattern-library/js/main.js
+++ pattern-library/js/main.js
@@ -8,22 +8,38 @@ $(function(){
 	}).end().append("<style>@media (min-width: 768px){ .available-devices ul { height: " + mostItems*1.8 + "rem; } }</style>");
 
 	// module23split video & module50pad video
-	$('.btnPlay').on('click', function() {
-		$(this).parent().fadeOut(800);
-		$(this).parent().next().attr('controls','1').get(0).play();
-		$(this).parent().next().addClass('show-controls');
-
-	});
-
-	// Module video play button behavior
-	$('.btnPlay').parent().next().bind('ended', function() {
-		$(this).prev().fadeIn(500);
-		$(this).load().removeAttr('controls').removeClass('show-controls');
-	});
-
-	$('.btnPlay').parent().next().bind('webkitendfullscreen', function() {
-		$(this).prev().fadeIn(500);
-		$(this).load().removeAttr('controls').removeClass('show-controls');
+	$('.btnPlay').on("click", function(e) {
+	    var element = $(this).parent().next();
+	    var vidId = $(element).data("vid-src");
+	    var youtube = '<iframe src="//www.youtube.com/embed/'+ vidId +'?autoplay=1&showinfo=0&modestbranding=1&rel=0&autohide=1" frameborder="0" allowfullscreen></iframe>';
+	    e.preventDefault();
+	    $(element).append(youtube).dialog({
+	    	dialogClass: "moduleVideo",
+	    	width: "75%",
+			resizable: false,
+			draggable: false,
+			modal: true,
+			show: {
+				effect: "fadeIn",
+				duration: 300
+			},
+			hide: {
+				effect: "fadeOut",
+				duration: 300
+			},
+			open: function(event, ui) { 
+				// Close dialog when outside is clicked
+				$('.ui-widget-overlay').bind('click', function(){ 
+					$(element).dialog('close'); 
+				}); 
+			},
+			close: function () {
+				// necessary as it stops the video when the dialog is closed
+				var iframe = $(this).children();
+				$(this).dialog('destroy');
+    			iframe.remove('iframe');
+			}
+	    });
 	});
 
 	// Slick Slider instantiation
